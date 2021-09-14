de61ca466473c05ef55eb56c81d55e3539897737
RaviAnand Mohabir <moravrav@gmailc.om>
2021-09-14 10:08:28 +0200

    🎉 Init Vue 3 project w/ TWCSS, ESLint & Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };

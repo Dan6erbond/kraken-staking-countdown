8b0ff6b45c9de4df94b6c3c3c9896f2d03e118ee
RaviAnand Mohabir <moravrav@gmail.com>
2021-09-27 18:05:18 +0200

    ♻️ Create separate header and currency selector components

---
+++ src/components/CurrencySelector.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  import { computed, toRefs } from "@vue/reactivity";
+  import { currency } from "../app/store";
+
+  // eslint-disable-next-line no-undef
+  const props = defineProps<{ currencies: string[] }>();
+  const { currencies: currs } = toRefs(props);
+
+  const currencies = computed(
+    () =>
+      currs.value &&
+      [...currs.value].sort((currency1) =>
+        currency.value.toLowerCase() === currency1.toLowerCase() ? -1 : 1,
+      ),
+  );
+</script>
+
+<template>
+  <div class="flex flex-col px-4 pt-4 space-y-4">
+    <span>Currency</span>
+    <div class="flex space-x-2 overflow-auto pb-4">
+      <div
+        v-for="curr in currencies"
+        :key="curr"
+        :class="[
+          'bg-dark-blue-700',
+          'hover:bg-dark-blue-600',
+          'rounded',
+          'shadow',
+          'px-2',
+          'py-1',
+          'cursor-pointer',
+          curr === currency && ['bg-dark-blue-900', 'hover:bg-dark-blue-800'],
+        ]"
+        @click="currency = curr"
+      >
+        {{ curr.toUpperCase() }}
+      </div>
+    </div>
+  </div>
+</template>

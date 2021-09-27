d26eeaa86befaadc23c3e45ec5223450e6246a8f
RaviAnand Mohabir <moravrav@gmail.com>
2021-09-27 15:29:27 +0200

    ✨ Improve currency formatting for small fractions

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,7 +1,7 @@
 export const formatAsCurrency = (value: number) => {
   return value.toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
+    minimumFractionDigits: value > 0.1 ? 2 : 5,
+    maximumFractionDigits: value > 0.1 ? 2 : 5,
   });
 };
 
